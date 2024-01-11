6242962c5bc1876d9ed395b50f1eaeff49d1f071
Ulifly <ulifly@hotmail.com>
2024-01-11 16:07:42 -0600

    se sgrega metodo de redondeo

--- 01/app.js
+++ 01/app.js
@@ -3,9 +3,9 @@
 
 
 let salarioBase = prompt('ingrese el salario base mensual');
-let salarioDiario = salarioBase / 30;
+let salarioDiario = (salarioBase / 30).toFixed(2);
 let diasTrabajados = prompt('ingrese los dias trabajados');
-let salarioMensual = salarioDiario * diasTrabajados;  
+let salarioMensual = (salarioDiario * diasTrabajados).toFixed(2);  
 document.write('el salario base mensual es: ' + salarioBase + '<br>' );
 document.write('los dias trabajados es: ' + diasTrabajados + '<br>' );
 document.write('el salario diario es de: ' + salarioDiario + '<br>' );
